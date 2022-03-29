b6f896c9cb98af453eb5594d144d4cdc93c7c3d8
molina <mateus.molinabr@gmail.com>
2022-03-29 08:57:25 +0200

    Adds tests 40 and 50

--- test21.mull_gtest/instructions.sh
+++
@@ -1,2 +0,0 @@
-clang-12 -fexperimental-new-pass-manager   -fpass-plugin=/lib/mull-ir-frontend-12   -g -grecord-command-line   main.cpp -o hello-world
-mull-runner-12 -ide-reporter-show-killed hello-world
--- test31.mull_gtest_cmake/build.dir/instructions.sh
+++
@@ -1,5 +0,0 @@
-rm -rf CMakeCache.txt CMakeFiles/ Makefile  cmake_install.cmake
-export CXX=clang++-12
-cmake  -DCMAKE_CXX_FLAGS="-O0 -fexperimental-new-pass-manager -fpass-plugin=/lib/mull-ir-frontend-12 -g -grecord-command-line"   ..
-make runTests -j
-mull-runner-12 --ld-search-path=/lib/x86_64-linux-gnu/ runTests
