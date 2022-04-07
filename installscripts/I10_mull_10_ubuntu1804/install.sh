5897df4faad1801f7ec3d94303e0b4a9a4fb218d
Mateus Molina <mateus.molinabr@gmail.com>
2022-04-07 15:16:49 +0200

    updates clang env I10, I12

--- installscripts/I10_mull_10_ubuntu1804/install.sh
+++ installscripts/I10_mull_10_ubuntu1804/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Clang env
-sudo apt-get install -y clang-10
+sudo apt-get install -y build-essential cmake llvm llvm-10-dev clang-10 libclang-10-dev 
 
 # Install Mull-10
 curl -1sLf 'https://dl.cloudsmith.io/public/mull-project/mull-stable/setup.deb.sh' | sudo -E bash
--- installscripts/I12_mull_ubuntu2004/install.sh
+++ installscripts/I12_mull_ubuntu2004/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install Clang env
-sudo apt-get install -y clang-12
+sudo apt-get install -y build-essential cmake llvm llvm-12-dev clang-12 libclang-12-dev 
 
 # Install Mull-12
 curl -1sLf 'https://dl.cloudsmith.io/public/mull-project/mull-stable/setup.deb.sh' | sudo -E bash
